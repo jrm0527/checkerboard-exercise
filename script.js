93c8baa2f14076ebfde863d909dcc0f103d47b73
jrm0527 <geeziegames@gmail.com>
2023-03-02 18:39:47 -0500

    refactored finished checkerboard

--- script.js
+++ script.js
@@ -6,30 +6,17 @@ function createSquare() {
   return div;
 }
 
-function createBlackBlock() {
-  const div = createSquare();
-  div.style.backgroundColor = "black";
-  return div;
-}
-
-function createRedBlock() {
-  const div = createSquare();
-  div.style.backgroundColor = "red";
-  return div;
-}
-
 function createCheckerBoard(size) {
   const board = document.createElement("div");
   for (let i = 0; i < size; i++) {
-    console.log(i);
+    const block = createSquare();
     if (i % 2 === 0) {
-      const block = createBlackBlock();
-      board.appendChild(block);
+      block.style.backgroundColor = "black";
     } else {
-      const block = createRedBlock();
-      board.appendChild(block);
+      block.style.backgroundColor = "red";
     }
+    board.appendChild(block);
   }
   return board;
 }
-document.body.appendChild(createCheckerBoard(81));
+document.body.appendChild(createCheckerBoard(63));
