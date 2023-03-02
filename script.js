a975304f9434d042889a8e95683458aca991654a
jrm0527 <geeziegames@gmail.com>
2023-03-02 17:19:49 -0500

    finished randomcolors

--- script.js
+++ script.js
@@ -3,32 +3,36 @@ function createSquare() {
   div.style.width = "11.1%";
   div.style.float = "left";
   div.style.paddingBottom = "11.1%";
+  const red = Math.random() * 255;
+  const green = Math.random() * 255;
+  const blue = Math.random() * 255;
+  div.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
   return div;
 }
 
-function createBlackBlock() {
-  const div = createSquare();
-  div.style.backgroundColor = "black";
-  return div;
-}
+// function createBlackBlock() {
+//   const div = createSquare();
+//   div.style.backgroundColor = "black";
+//   return div;
+// }
 
-function createRedBlock() {
-  const div = createSquare();
-  div.style.backgroundColor = "red";
-  return div;
-}
+// function createRedBlock() {
+//   const div = createSquare();
+//   div.style.backgroundColor = "red";
+//   return div;
+// }
 
 function createCheckerBoard(size) {
   const board = document.createElement("div");
   for (let i = 0; i < size; i++) {
-    console.log(i);
-    if (i % 2 === 0) {
-      const block = createBlackBlock();
-      board.appendChild(block);
-    } else {
-      const block = createRedBlock();
-      board.appendChild(block);
-    }
+    // if (i % 2 === 0) {
+    //   const block = createBlackBlock();
+    //   board.appendChild(block);
+    // } else {
+    //   const block = createRedBlock();
+    //   board.appendChild(block);
+    // }
+    board.appendChild(createSquare());
   }
   return board;
 }
